7cc29c1e431ba0529bd3098e434841e0665e4022
glopezgaschetto <gonzalolg.lopez@gmail.com>
2022-09-22 21:25:27 -0300

    agrego tostify

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
 document.getElementById("formulario").addEventListener("submit", guardarTarea);
+let formulario_btn = document.getElementById("formulario_btn");
+
 
 function guardarTarea(e) {
     let titulo = document.getElementById("titulo").value;
     let descripcion = document.getElementById("descripcion").value;
+
+
 
     let tarea = {
         titulo,
         descripcion
     };
 
-    if (localStorage.getItem("tareas") === null) {
-        let tareas = [];
-        tareas.push(tarea);
-        localStorage.setItem("tareas", JSON.stringify(tareas));
-    }else{
-        let tareas = JSON.parse(localStorage.getItem("tareas"));
-        tareas.push(tarea);
-        localStorage.setItem("tareas", JSON.stringify(tareas));
-    }
-    crearTareas();
-    Toastify({
-        text: "NOTA CREADA EXISTOSAMENTE",
-        className: "info",
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
+   
+
+
+        if (localStorage.getItem("tareas") === null) {
+            let tareas = [];
+            tareas.push(tarea);
+            localStorage.setItem("tareas", JSON.stringify(tareas));
+    
+        } else {
+            let tareas = JSON.parse(localStorage.getItem("tareas"));
+            tareas.push(tarea);
+            localStorage.setItem("tareas", JSON.stringify(tareas));
         }
-      }).showToast();
+        
+        
+        Toastify({
+            text: "NOTA CREADA EXISTOSAMENTE",
+            duration: 3000 
+            }).showToast();
+        
+        
+        
+        crearTareas();
+    
+    
+
+
+
     document.getElementById("formulario").reset();
     e.preventDefault();
 }
@@ -35,17 +50,18 @@ function crearTareas() {
     let tareas = JSON.parse(localStorage.getItem("tareas"));
     let mostrarTareas = document.getElementById("mostrarNotas");
 
+
     mostrarTareas.innerHTML = "";
 
     for (let i = 0; i < tareas.length; i++) {
-       let titulo = tareas[i].titulo;
-       let descripcion = tareas[i].descripcion;
+        let titulo = tareas[i].titulo;
+        let descripcion = tareas[i].descripcion;
 
-        mostrarTareas.innerHTML += 
-        `<div class="vannotas" id="vannotas"><p>${titulo}</p><p>${descripcion}</p>
+        mostrarTareas.innerHTML +=
+            `<div class="vannotas" id="vannotas"><p>${titulo}</p><p>${descripcion}</p>
             <input type="button" value="BORRAR" onclick="borrarNota('${titulo}')" >
         </div>`;
-        
+
     }
 
 
@@ -53,12 +69,12 @@ function crearTareas() {
 
 function borrarNota(titulo) {
     let tareas = JSON.parse(localStorage.getItem("tareas"));
-  for(let i = 0; i < tareas.length; i++) {
-    if(tareas[i].titulo == titulo) {
-      tareas.splice(i, 1);
+    for (let i = 0; i < tareas.length; i++) {
+        if (tareas[i].titulo == titulo) {
+            tareas.splice(i, 1);
+        }
     }
-  }
-  localStorage.setItem("tareas", JSON.stringify(tareas));
-  crearTareas();
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    crearTareas();
 }
 crearTareas();
