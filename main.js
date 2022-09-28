e7f6c03cefbd7514a1e5cb81d58fe6aea009c311
glopezgaschetto <gonzalolg.lopez@gmail.com>
2022-09-28 11:20:02 -0300

    modificacion

--- main.js
+++ main.js
@@ -1,49 +1,78 @@
 document.getElementById("formulario").addEventListener("submit", guardarTarea);
 let formulario_btn = document.getElementById("formulario_btn");
+let titulo = document.getElementById("titulo").value;
+let descripcion = document.getElementById("descripcion").value;
 
 
-function guardarTarea(e) {
-    let titulo = document.getElementById("titulo").value;
-    let descripcion = document.getElementById("descripcion").value;
-
-
-
-    let tarea = {
-        titulo,
-        descripcion
-    };
-
-   
-
-
-        if (localStorage.getItem("tareas") === null) {
-            let tareas = [];
-            tareas.push(tarea);
-            localStorage.setItem("tareas", JSON.stringify(tareas));
     
-        } else {
-            let tareas = JSON.parse(localStorage.getItem("tareas"));
-            tareas.push(tarea);
-            localStorage.setItem("tareas", JSON.stringify(tareas));
-        }
+    function guardarTarea(e) {
+        let titulo = document.getElementById("titulo").value;
+        let descripcion = document.getElementById("descripcion").value;
+    
+        let tarea = {
+            titulo,
+            descripcion
+        };
         
         
-        Toastify({
-            text: "NOTA CREADA EXISTOSAMENTE",
-            duration: 3000 
-            }).showToast();
+            
+    
+        if ((titulo && descripcion ) == "") {
+            Toastify({
+                text: "NO HAS CREADO UNA NOTA",
+                className: "warning",
+                style: {
+                    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                  },               
+                duration: 3000
+            }).showToast() }else{
+
+                if (localStorage.getItem("tareas") === null) {
+                    let tareas = [];
+                    tareas.push(tarea);
+                    localStorage.setItem("tareas", JSON.stringify(tareas));
+        
+                } else {
+                    let tareas = JSON.parse(localStorage.getItem("tareas"));
+                    tareas.push(tarea);
+                    localStorage.setItem("tareas", JSON.stringify(tareas));
+                }
+        
+        
+                Toastify({
+                    text: "NOTA CREADA EXISTOSAMENTE",
+                    duration: 3000
+                }).showToast();
         
         
         
-        crearTareas();
+                crearTareas();
+
+
+            }
     
     
+    
+    
+    
+    
+    
+    
+        
+    
+    
+    
+    
+    
+        document.getElementById("formulario").reset();
+        e.preventDefault();
+    }
 
 
 
-    document.getElementById("formulario").reset();
-    e.preventDefault();
-}
+
+
+
 
 function crearTareas() {
 
